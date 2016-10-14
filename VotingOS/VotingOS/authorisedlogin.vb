d900faabaaa305ef1de25c8af0f8830f61ed3657
Jason Jay Dookarun <jason.dookarun@live.co.uk>
2016-10-14 18:00:20 +0100

    Changes have been made w/ student form deleted. Sync complete w/ HP

--- VotingOS/VotingOS/authorisedlogin.vb
+++ VotingOS/VotingOS/authorisedlogin.vb
@@ -8,7 +8,7 @@ Public Class authorisedlogin
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         provider = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source ="
         'Change the following to your access database location
-        dataFile = "N:\Voting System\VotingOS\VotingOS\bin\Debug\authorisedmembers.accdb"
+        dataFile = "C:\Users\J Dookarun\Voting System\VotingOS\VotingOS\bin\Debug\authorisedmembers.accdb"
         connString = provider & dataFile
         myConnection.ConnectionString = connString
 
