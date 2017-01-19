25fdd7cd8c4123773c8823fee002898b122c3cf6
Jason Jay Dookarun <jason.dookarun@live.co.uk>
2017-01-19 17:21:56 +0000

    Change in application look is to take place

--- VotingOS/VotingOS/MainLogin.vb
+++ VotingOS/VotingOS/MainLogin.vb
@@ -15,7 +15,6 @@ Public Class MainLogin
         myConnection.Open()
         Dim cmd As OleDbCommand = New OleDbCommand("SELECT * FROM [users] WHERE [user] = '" & TextBox1.Text & "' AND [password] = '" & TextBox2.Text & "' AND [YearGroup] & '" & TextBox3.Text & "'", myConnection)
         Dim dr As OleDbDataReader = cmd.ExecuteReader
-        TextBox1.Text = username
 
         ' the following variable is hold true if user is found, and false if user is not found
         Dim userFound As Boolean = False
@@ -32,6 +31,7 @@ Public Class MainLogin
 
         'checking the result: IF FOUND, THE PIECE IS DIRECTED FOR THE STUDENTS DIRECTLY TO THE VOTING PAGE OS (STUDENTS ONLY_)
         If userFound = True Then
+            MsgBox("Hi", username)
             Me.Hide()
             votingchoice.Show()
 
